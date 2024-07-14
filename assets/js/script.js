0e4360eee19cfc8daef0ffd981fc315c3ef7d9da
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-07-15 02:57:42 +0500

    Work Sample Section added!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+// Toggle FAQs
 const heroFAQs = document.querySelectorAll(
   ".hero-section .hero-faqs > ul > li"
 );
 heroFAQs.forEach((faq, index) => {
   const ques = faq.querySelector("div.ques");
   ques.addEventListener("click", () => {
-    console.log("hihihi");
     heroFAQs.forEach((_faq, _index) => {
       if (index === _index) {
         _faq.classList.toggle("active");
@@ -12,5 +12,42 @@ heroFAQs.forEach((faq, index) => {
         _faq.classList.remove("active");
       }
     });
+  });
+});
+
+// Filtering NFTs by Timelines
+const allFilterBtns = document.querySelectorAll(
+  ".work_type_filter_btn[data-filter]"
+);
+$(document).ready(function () {
+  $(".work_type_filter_btn").click(function () {
+    var value = $(this).attr("data-filter");
+
+    if (value == "all_projects") {
+      $(".work_sample_filter_card").show("1000");
+    } else {
+      $(".work_sample_filter_card")
+        .not("." + value)
+        .hide("3000");
+      $(".work_sample_filter_card")
+        .filter("." + value)
+        .show("3000");
+    }
+  });
+
+  if ($(".work_type_filter_btn").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
+  allFilterBtns[0].click();
+});
+
+allFilterBtns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    allFilterBtns.forEach((btn, i) => {
+      btn.classList.remove("btn-light");
+      btn.classList.add("btn-dark");
+    });
+    btn.classList.add("btn-light");
   });
 });
